25dce8cfa250a577fd249589c1e96f971b68974e
kishor <kishorrathva8298@gmail.com>
2018-10-07 05:29:52 +0530

    ex1

---
+++ Exercise 1/Exercise1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*This is a comment. */
+
+int main(int args,char *argv[]){
+	int distance = 100;
+	// this is also a comment
+
+	printf("You are %d mile away",distance );
+
+	return 0;
+	
+}
