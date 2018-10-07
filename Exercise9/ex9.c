3b37edf486c7d4397453e7e4ab678ba6761d8bac
kishor <kishorrathva8298@gmail.com>
2018-10-08 04:30:17 +0530

    ex9

--- Exercise9/ex9.c
+++ Exercise9/ex9.c
@@ -1,11 +1,24 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main(int argc, char *argv[])
-{
-	int i=0;
-	while(i<25){
-		printf("%d",i );
-		i++;
+int main(int argc , char *argv[]){
+	int i =0;
+
+	//go through each string in argv
+	for(i=1;i<argc;i++){
+		printf("arg %d: %s\n",i, argv[i] );
 	}
+	//our own array of strings
+	char *states[] = {
+		"Delhi","Ahmedabad",
+		"Mumbai","Surat"
+	};
+
+	int num_states = 4;
+
+	for(i=0;i<num_states;i++){
+		printf("state %d : %s\n", i ,states[i]);
+
+	}
+
 	return 0;
 }
